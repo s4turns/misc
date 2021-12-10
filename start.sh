779c259c94fcfbb3159d0999103a5a2f2a4577d5
dnlsplnd <81465977+dnlsplnd@users.noreply.github.com>
2021-12-10 03:01:11 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,11 @@ sudo apt update &&
 sudo apt upgrade -y &&
 sudo apt dist-upgrade &&
 sudo apt install tcl-tls tcl-dev tk-dev mesa-common-dev libjpeg-dev libtogl-dev build-essential -y &&
-sudo apt install libc6-dev libssl-dev libperl-dev pkg-config libicu-dev sudo fail2ban ccache dstat tmux iptraf-ng glances libwww-perl git -y
+sudo apt install libc6-dev libssl-dev libperl-dev pkg-config libicu-dev sudo fail2ban ccache dstat tmux iptraf-ng glances libwww-perl git -y &&
+sudo apt install haveged ufw psmisc net-tools ca-certificates git dnsutils -y &&
+sudo apt autoremove -y
 
 echo "[!] That shit is fuckin READY MUDDA FUCKA!"
+echo ".:WiTH L0VE FR0M BLCKND CREW:."
+
+done
