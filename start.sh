de594519cbddc8f3285f62fb300f70315412a7e1
s4turns <53133058+s4turns@users.noreply.github.com>
2021-12-09 20:24:58 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 sudo apt update &&
 sudo apt upgrade -y &&
 sudo apt dist-upgrade &&
-sudo apt install tcl-tls tcl-dev tk-dev mesa-common-dev libjpeg-dev libtogl-dev build-essential -y &&
+sudo apt install tcl-tls tcl-dev tk-dev mesa-common-dev libjpeg-dev libtogl-dev build-essential libssl-dev -y &&
 sudo apt install libc6-dev libssl-dev libperl-dev pkg-config libicu-dev sudo fail2ban ccache dstat tmux iptraf-ng libwww-perl git -y &&
 sudo apt install haveged ufw psmisc net-tools ca-certificates git dnsutils -y &&
 sudo apt autoremove -y
