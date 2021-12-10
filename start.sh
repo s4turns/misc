e5275adab0bfdab85d1f26372616caa5423963b0
s4turns <53133058+s4turns@users.noreply.github.com>
2021-12-09 19:46:18 -0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt update &&
+sudo apt upgrade -y &&
+sudo apt dist-upgrade &&
+sudo apt install tcl-tls tcl-dev tk-dev mesa-common-dev libjpeg-dev libtogl-dev build-essential -y &&
+sudo apt install libc6-dev libssl-dev libperl-dev pkg-config libicu-dev sudo fail2ban ccache dstat tmux iptraf-ng glances libwww-perl git -y
+
+echo "[!] That shit is fuckin READY MUDDA FUCKA!"
+
+done
