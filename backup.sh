6a79ecbfe018d41b9a229d7d2c2a1de747270e72
s4turns <53133058+s4turns@users.noreply.github.com>
2021-12-12 20:40:02 -0600

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -9,5 +9,3 @@ output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 tar -czf $output $input 2> /dev/null
 echo "Backup of $input completed! Details about the output backup file:"
 ls -l $output
-
-done
