f1f5485801816afea677b0799003feb6cf230a99
s4turns <53133058+s4turns@users.noreply.github.com>
2021-12-14 14:19:45 -0600

    Update node.update.sh

--- node.update.sh
+++ node.update.sh
@@ -1,6 +1,11 @@
 sudo apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm ls-remote
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 echo What version do you want to install?
 
 read ver
