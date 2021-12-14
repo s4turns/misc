a39c88ac80150dea94a4ec3dd15ee1efe9b007af
s4turns <53133058+s4turns@users.noreply.github.com>
2021-12-14 16:10:56 -0600

    Create x.sh

---
+++ x.sh
@@ -0,0 +1,20 @@
+tcpdump -r dump.191158.cap |less
+
+interface=ens3
+dumpdir=/var/log
+
+while /bin/true; do
+  pkt_old=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $2 }'`
+  sleep 1
+  pkt_new=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $2 }'`
+
+  pkt=$(( $pkt_new - $pkt_old ))
+  echo -ne "\r$pkt packets/s\033[0K"
+
+  if [ $pkt -gt 5000 ]; then
+    echo "\nOMFG, DoS detected!!!!!@#$%^&* no1curr."
+    tcpdump -n -s0 -c 5000 -w dump.`date +"%H%M%S"`.cap
+    echo "Going to sleep for 5 minutes."
+    sleep 300
+  fi
+done
