8ac565ed34e85ab1fc5d69f7a33e80a96fb9de0c
s4turns <53133058+s4turns@users.noreply.github.com>
2021-12-14 15:14:02 -0600

    Update nvm.sh

--- nvm.sh
+++ nvm.sh
@@ -2,11 +2,13 @@
 
 sudo apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-nvm ls-remote
-
+sleep 5
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm ls-remote
+sleep 5
 
 echo What version do you want to install?
 
