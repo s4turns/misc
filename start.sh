3520110e1dfb709c3729b9cb439f9e02a5da7b29
s4turns <53133058+s4turns@users.noreply.github.com>
2021-12-19 05:46:47 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,5 +8,4 @@ sudo apt install libc6-dev libssl-dev libperl-dev pkg-config libicu-dev sudo fai
 sudo apt install haveged ufw psmisc net-tools ca-certificates git dnsutils -y &&
 sudo apt autoremove -y
 
-echo "[!] That shit is fuckin READY MUDDA FUCKA!"
 echo ".:WiTH L0VE FR0M BLCKND CREW:."
