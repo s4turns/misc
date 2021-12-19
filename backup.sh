5581c8d506566781efc185b64a5eb9a0a516fcd4
s4turns <53133058+s4turns@users.noreply.github.com>
2021-12-19 05:47:24 -0600

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -9,3 +9,4 @@ output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 tar -czf $output $input 2> /dev/null
 echo "Backup of $input completed! Details about the output backup file:"
 ls -l $output
+echo ".:WiTH L0VE FR0M BLCKND CREW:."
