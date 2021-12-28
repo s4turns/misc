a9d8b4090a0ba4fd4feea67fb458bc84433de706
s4turns <53133058+s4turns@users.noreply.github.com>
2021-12-28 04:28:08 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ sudo apt upgrade -y &&
 sudo apt dist-upgrade &&
 sudo apt install tcl-tls tcl-dev tk-dev mesa-common-dev libjpeg-dev libtogl-dev build-essential -y &&
 sudo apt install libc6-dev libssl-dev libperl-dev pkg-config libicu-dev sudo fail2ban ccache dstat tmux iptraf-ng libwww-perl git -y &&
-sudo apt install haveged ufw psmisc net-tools ca-certificates git dnsutils -y &&
+sudo apt install haveged ufw psmisc net-tools ca-certificates git dnsutils curl -y &&
 sudo apt autoremove -y
 
 echo ".:WiTH L0VE FR0M BLCKND CREW:."
