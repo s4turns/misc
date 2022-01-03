92c717f7ecbecf4041e1b68f5d8507573cc6ff15
s4turns <53133058+s4turns@users.noreply.github.com>
2022-01-03 15:59:02 -0600

    Update pi4_teamspeak64.sh

--- pi4_teamspeak64.sh
+++ pi4_teamspeak64.sh
@@ -6,7 +6,7 @@ sudo apt install git build-essential cmake screen -y
 git clone --branch "v0.2.2" https://github.com/ptitSeb/box86
 sudo dpkg --add-architecture armhf
 sudo apt update
-sudo apt install gcc-arm-linux-gnueabihf libc6:armhf libncurses5:armhf libstdc++6:armhf
+sudo apt install gcc-arm-linux-gnueabihf libc6:armhf libncurses5:armhf libstdc++6:armhf -y
 cd ~/box86
 mkdir build
 cd build
