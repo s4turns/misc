1b6a7af36f46642455edef85f4143f6cb7964753
s4turns <53133058+s4turns@users.noreply.github.com>
2022-01-04 04:45:41 -0600

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -4,10 +4,11 @@ cd monero
 wget https://github.com/xmrig/xmrig/releases/download/v6.6.0/xmrig-6.6.0-linux-x64.tar.gz
 tar -xvf xmrig-6.6.0-linux-x64.tar.gz
 cd xmrig-6.6.0/
+
+echo  ****change pool address and wallet address*****
+echo xmr.2miners.com:2222
+
 nano config.json
 wait
-
-# ****change pool address and wallet address*****
-# xmr.2miners.com:2222
 
 echo now ./xmrig
